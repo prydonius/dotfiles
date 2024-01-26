e98c3ae3de7d4ba375ef6a2e8542b53b48b7511d
Adnan Abdulhussein <adnan@prydoni.us>
2024-01-25 17:39:30 -0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,22 +2,26 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 eval "$(/opt/homebrew/bin/brew shellenv)"
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+end
 
 # fnm node
-fnm env --use-on-cd | source
+if type -q fnm
+    fnm env --use-on-cd | source
+end
 
 # SDKMan
 function sdk
     bash -c "source '$HOME/.sdkman/bin/sdkman-init.sh'; sdk $argv[1..]"
 end
 
-fish_add_path (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
+if test -d $HOME/.sdkman
+    fish_add_path (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
+end
 fish_add_path /opt/homebrew/opt/python@3.10/libexec/bin
 fish_add_path "/Users/adnan/Library/Application Support/JetBrains/Toolbox/scripts"
-
-# Enable git plugin
-__git.reset
 
 abbr --add k kubectl
 abbr --add kctx kubectx
